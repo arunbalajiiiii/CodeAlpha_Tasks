4179445f15b0caa90fe986569357bbbec2653289
arunbalajiiiii <arunbalaji232@gmail.com>
2026-06-26 13:15:11 +0530

    security: move firebase config to environment variables and flatten repository structure

--- project/CodeAlpha-task-master/frontend/src/firebase.js
+++ project/CodeAlpha-task-master/frontend/src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { isSupported, getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIPF5dzQZ0nB_xaMk80uJaHntWZ-OXkow",
-  authDomain: "project-management-1239a.firebaseapp.com",
-  projectId: "project-management-1239a",
-  storageBucket: "project-management-1239a.firebasestorage.app",
-  messagingSenderId: "1032112269461",
-  appId: "1:1032112269461:web:8d0aabf5e663557468939c",
-  measurementId: "G-FZHH18MY3X"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
